41976b7a182bcd7fadf917da20cb3edc81cc1e62
etoyadadaya <72203330+etoyadadaya@users.noreply.github.com>
2022-10-01 17:58:30 +0700

    Update selectionSort.ts

--- selectionSort.ts
+++ selectionSort.ts
@@ -1,7 +1,7 @@
 // ascending order
 // [1,3,5,7,9,0,2,4,6,8] => [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-const selectionSort = (arr: number[]) => {
+const selectionSortAscending = (arr: number[]) => {
     for (let i = 0; i < arr.length; i++) {
         let indexMin = i;
         for (let j = i + 1; j < arr.length; j++) {
@@ -16,15 +16,15 @@ const selectionSort = (arr: number[]) => {
     return arr;
 }
 
-const arr = [1,3,5,7,9,0,2,4,6,8];
+const arrAscending = [1,3,5,7,9,0,2,4,6,8];
 
-console.log(selectionSort(arr));
+console.log(selectionSortAscending(arrAscending));
 
                                        
 // descending order
 // [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  => [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
                                        
-const selectionSort = (arr: number[]) => {
+const selectionSortDescending  = (arr: number[]) => {
     for (let i = 0; i < arr.length; i++) {
         let indexMin = i;
         for (let j = i + 1; j < arr.length; j++) {
@@ -39,6 +39,6 @@ const selectionSort = (arr: number[]) => {
     return arr;
 }
 
-const arr = [1,3,5,7,9,0,2,4,6,8];
+const arrDescending = [1,3,5,7,9,0,2,4,6,8];
 
-console.log(selectionSort(arr));                                       
+console.log(selectionSortDescending(arrDescending));         

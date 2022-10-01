97082f4fa6f3d9f4489c685914bd66a0e907fc2c
etoyadadaya <72203330+etoyadadaya@users.noreply.github.com>
2022-10-01 10:34:14 +0700

    Update binarySearch.js

--- binarySearch.js
+++ binarySearch.js
@@ -37,3 +37,37 @@ var solution = function(isBadVersion: any) {
         return high + 1;
     };
 };
+
+
+//leetcode 33. Search in Rotated Sorted Array
+
+/*
+When you divide the rotated array into two halves, using mid index, at least one of subarray should remain sorted ALWAYS.
+*/
+
+function search(nums: number[], target: number): number {
+    let low = 0;
+    let high = nums.length - 1;
+
+    while (low <= high) {
+        let mid = Math.floor((low + high) / 2);
+
+        if (nums[mid] === target) {
+            return mid;
+        } else if (nums[low] <= nums[mid]) {
+            if (nums[low] <= target && target <= nums[mid]) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        } else {
+            if (nums[mid] <= target && target <= nums[high]) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+    }
+
+    return -1;
+};

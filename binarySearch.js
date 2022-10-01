4473d6c2e808e135338af849ddb048f54dd50fde
etoyadadaya <72203330+etoyadadaya@users.noreply.github.com>
2022-10-01 09:55:13 +0700

    Implemented binarySearch

---
+++ binarySearch.js
@@ -0,0 +1,19 @@
+//leetcode 704. Binary Search
+
+function search(nums: number[], target: number): number {
+    let low = 0;
+    let high = nums.length - 1;
+    
+    while (low <= high) {
+        let mid = Math.floor((low + high) / 2);
+        
+        if (nums[mid] === target) {
+            return mid;
+        } else if (nums[mid] < target) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+    return -1;
+};

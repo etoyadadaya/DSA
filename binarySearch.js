fb8171e0f60255c9989e97ccba24ec9559207ab8
etoyadadaya <72203330+etoyadadaya@users.noreply.github.com>
2022-10-01 10:17:08 +0700

    Update binarySearch.js

--- binarySearch.js
+++ binarySearch.js
@@ -17,3 +17,23 @@ function search(nums: number[], target: number): number {
     }
     return -1;
 };
+
+
+//leetcode 278. First Bad Version
+
+var solution = function(isBadVersion: any) {
+    return function (n: number): number {
+        let low = 1;
+        let high = n;
+
+        while (low <= high) {
+            let mid = Math.floor((low + high) / 2);
+            if(isBadVersion(mid)) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+    }
+        return high + 1;
+    };
+};

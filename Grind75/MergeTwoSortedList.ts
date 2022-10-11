3ebb6dd76f948966e0bee49cd5c5da2ba453f128
Ivan Denisenko <ticklere@MacBook-Air.local>
2022-10-11 20:19:49 +0700

    day1

---
+++ Grind75/MergeTwoSortedList.ts
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
+  let dummy = new ListNode(0);
+  let head = dummy;
+
+  while (list1 && list2) {
+    if (list1.val <= list2.val) {
+      dummy.next = list1;
+      list1 = list1.next;
+    } else {
+      dummy.next = list2;
+      list2 = list2.next;
+    }
+    dummy = dummy.next;
+  }
+
+  if (list1 !== null) {
+    dummy.next = list1;
+  } else {
+    dummy.next = list2;
+  }
+
+  return head.next;
+}
---
+++ Grind75/ValidParentheses.ts
@@ -0,0 +1,19 @@
+function isValid(s: string): boolean {
+
+  let stack = [];
+
+  for (let i = 0; i < s.length; i++) {
+    let char = s[i];
+    if (char === "(" || char === "{" || char === "[") {
+      stack.push(char);
+    } else {
+      let prevValue = stack.pop();
+
+      if (prevValue === "(" && char !== ")" ) return false;
+      if (prevValue === "{" && char !== "}") return false;
+      if (prevValue === "[" && char !== "]") return false;
+      if (prevValue === undefined) return false;
+    }
+  }
+  return stack.length === 0;
+};
